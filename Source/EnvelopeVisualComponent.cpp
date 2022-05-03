dd5bf85a8a71618d83a3086718897938a4e07a10
Alway <72552510+Alway09@users.noreply.github.com>
2022-05-03 21:47:46 +1000

    Ограничения на перемещение точек
    Добавил ограничения на перемещение точек в классе EnvelopeVisualComponent

--- Source/EnvelopeVisualComponent.cpp
+++ Source/EnvelopeVisualComponent.cpp
@@ -10,6 +10,7 @@
 
 #include <JuceHeader.h>
 #include "EnvelopeVisualComponent.h"
+#include <limits>
 
 //==============================================================================
 EnvelopeVisualComponent::EnvelopeVisualComponent() :
@@ -59,40 +60,157 @@ void EnvelopeVisualComponent::mouseDrag(const juce::MouseEvent& event) {
     MovedDot* dot = dynamic_cast<MovedDot*>(event.eventComponent);
 
     if (dot != nullptr) {
-        auto relativeEvent = event.getEventRelativeTo(this);
-
-        dot->setCentrePosition(relativeEvent.getPosition());
+        auto pos = event.getEventRelativeTo(this).getPosition();
+        juce::Point<int> centre = stayPointInsideComponent(dot, pos);
+        centre = complyOrder(dot, centre);
+        dot->setCentrePosition(centre);
     }
 }
 
+juce::Point<int> EnvelopeVisualComponent::stayPointInsideComponent(MovedDot* dot, juce::Point<int> eventPos) {
+    juce::Point<int> centre;
+
+    if (eventPos.getX() < 0) {
+        centre.setX(0);
+    }
+    else if (eventPos.getX() > getWidth()) {
+        centre.setX(getWidth());
+    }
+    else {
+        centre.setX(eventPos.getX());
+    }
+
+    if (eventPos.getY() < 0) {
+        centre.setY(0);
+    }
+    else if (eventPos.getY() > getHeight()) {
+        centre.setY(getHeight());
+    }
+    else {
+        centre.setY(eventPos.getY());
+    }
+
+    return centre;
+}
+
+juce::Point<int> EnvelopeVisualComponent::complyOrder(MovedDot* dot, juce::Point<int> eventPos) {
+    int leftIndex = getDotIndex(dot->getLeftId());
+    int rigthIndex = getDotIndex(dot->getRightId());
+
+    //int leftX = 0;
+    //int rightX = getWidth();
+    juce::Point<int> dotCentre(eventPos.getX(), eventPos.getY());
+
+    if (leftIndex != -1) {
+        int leftX = dotsVector[leftIndex]->getX() + dotsVector[leftIndex]->getWidth() / 2;
+        if (dotCentre.getX() < leftX) {
+            dotCentre.setX(leftX);
+            //dot->setCentrePosition(dotCentre);
+
+            //return;
+        }
+    }
+
+    if (rigthIndex != -1) {
+        //DBG(rigthIndex);
+        int rightX = dotsVector[rigthIndex]->getX() + dotsVector[rigthIndex]->getWidth() / 2;
+        if (dotCentre.getX() > rightX) {
+            dotCentre.setX(rightX);
+            //dot->setCentrePosition(dotCentre);
+
+            //return;
+        }
+    }
+
+    return dotCentre;
+}
+
 void EnvelopeVisualComponent::addDot(int x, int y) {
-    MovedDot* dot = new MovedDot(dotsAmount, true);
+    std::vector<MovedDot*>::iterator iterStart = dotsVector.begin();
+    int leftID = 0; 
+    //int rightID = std::numeric_limits<int>::max();
+
+    while (iterStart != dotsVector.end()) { // find left dot
+        if ((*iterStart)->getPosition().getX() + (*iterStart)->getWidth() < x) {
+            leftID = (*iterStart)->getId();
+            //(*iterStart)->setRightId(dotsIdConted);
+        }
+
+        ++iterStart;
+    }
+
+    if(leftID != 0)
+        dotsVector[getDotIndex(leftID)]->setRightId(dotsIdConted);
+
+    std::vector<MovedDot*>::iterator iterEnd = dotsVector.end(); 
+    int rightID = std::numeric_limits<int>::max();
+
+    while (iterEnd != dotsVector.begin()) { // find rigth dot
+        --iterEnd; // cause end() iter is out of bounds
+
+        if ((*iterEnd)->getPosition().getX() + (*iterEnd)->getWidth() > x) {
+            rightID = (*iterEnd)->getId();
+            //(*iterEnd)->setLeftId(dotsIdConted);
+        }   
+    }
+
+    if(rightID != std::numeric_limits<int>::max())
+        dotsVector[getDotIndex(rightID)]->setLeftId(dotsIdConted);
+
+    MovedDot* dot = new MovedDot(dotsIdConted, leftID, rightID, true);
+
+    DBG(dotsIdConted);
+    DBG(leftID);
+    DBG(rightID);
+    DBG("-----------");
+
     dotsVector.push_back(dot);
-    ++dotsAmount;
+    //dotsIDs.push_back(dotsAmount);
+
+    ++dotsIdConted;
 
     dot->setCentrePosition(x, y);
-
     dot->addMouseListener(this, false);
     addAndMakeVisible(dot);
 }
 
 void EnvelopeVisualComponent::removeDot(int ID) {
+    int dotIndex = getDotIndex(ID);  
+    
+    if (dotIndex != -1) {
+        int leftDotIndex = getDotIndex(dotsVector[dotIndex]->getLeftId());
+        int rightDotIndex = getDotIndex(dotsVector[dotIndex]->getRightId());
 
-    std::vector<MovedDot*>::iterator iter(dotsVector.begin());
+        if (leftDotIndex != -1 && rightDotIndex != -1) {
+            dotsVector[leftDotIndex]->setRightId(dotsVector[rightDotIndex]->getId()); //connect before erasing
+            dotsVector[rightDotIndex]->setRightId(dotsVector[leftDotIndex]->getId());
+        } 
 
-    while(iter != dotsVector.end())
-    {
-        if ((*iter)->getId() == ID) {
-            delete (*iter);
-            dotsVector.erase(iter);
-            break;
-        }
-
-        ++iter;
+        delete *(dotsVector.begin() + dotIndex);
+        dotsVector.erase(dotsVector.begin() + dotIndex);
+        //dotsIDs.erase(dotsIDs.begin() + dotIndex);
     }
 }
 
-MovedDot::MovedDot(int ID, bool doubleClickAllow) : juce::Component(), dotId(ID), doubleClickAllowed(doubleClickAllow) , mouseIsOn(true)
+int EnvelopeVisualComponent::getDotIndex(int ID) {
+    std::vector<MovedDot*>::iterator iter(dotsVector.begin());
+    int counter = 0;
+
+    while (iter != dotsVector.end()) {
+        if ((*iter)->getId() == ID) {
+            return counter;
+        }
+
+        ++iter;
+        ++counter;
+    }
+
+    return -1;
+}
+
+MovedDot::MovedDot(int ID, int leftID, int rightID, bool doubleClickAllow) : 
+    juce::Component(), dotId(ID), doubleClickAllowed(doubleClickAllow) , 
+    mouseIsOn(true), leftDotId(leftID), rightDotId(rightID)
 {
     localBounds = juce::Rectangle<int>(UI::ADSR::movedDotAreaSize, UI::ADSR::movedDotAreaSize);
     dotBounds = juce::Rectangle<int>(UI::ADSR::movedDotDiameter, UI::ADSR::movedDotDiameter);
--- Source/EnvelopeVisualComponent.h
+++ Source/EnvelopeVisualComponent.h
@@ -17,7 +17,7 @@
 class MovedDot : public juce::Component
 {
 public:
-    MovedDot(int ID, bool doubleClickAllow);
+    MovedDot(int ID, int leftID, int rightID, bool doubleClickAllow);
     ~MovedDot() {};
 
     void paint(juce::Graphics&) override;
@@ -28,13 +28,20 @@ public:
     //void mouseDoubleClick(const juce::MouseEvent& event) override;
 
     int getId() const { return dotId; }
-    //void setAreaCentre(int x, int y);
+    int getLeftId() const { return leftDotId; }
+    int getRightId() const { return rightDotId; }
+    //juce::Point<int> getCentre() const { return localBounds.getCentre(); }
     void updateBounds();
+
+    void setLeftId(int ID) { leftDotId = ID; }
+    void setRightId(int ID) { rightDotId = ID; }
 
 private:
     bool doubleClickAllowed;
     bool mouseIsOn;
     int dotId;
+    int leftDotId;
+    int rightDotId;
 
     juce::Rectangle<int> localBounds;
     juce::Rectangle<int> dotBounds;
@@ -55,12 +62,16 @@ public:
 
     void addDot(int x, int y);
     void removeDot(int ID);
+    int getDotIndex(int ID);
 
+    juce::Point<int> stayPointInsideComponent(MovedDot* dot, juce::Point<int> eventPos);
+    juce::Point<int> complyOrder(MovedDot* dot, juce::Point<int> eventPos);
 private:
     //bool doubleClickAllowed;
-    int dotsAmount = 0;
+    int dotsIdConted = 1;
 
     std::vector<MovedDot*> dotsVector;
+    //std::vector<int> dotsIDs;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EnvelopeVisualComponent)
 };
